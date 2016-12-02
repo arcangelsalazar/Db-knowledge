e8f12564b17577925fe2ea8916389f645022c5cf
Arcangel <Arcangel@RUBY>
2016-12-02 17:46:47 -0600

    Se agrega al proyecto.

--- db-knowledge/src/main/java/mx/edu/itsm/msc/agenteNotificador/Notificador.java
+++ db-knowledge/src/main/java/mx/edu/itsm/msc/agenteNotificador/Notificador.java
@@ -5,19 +5,17 @@
  */
 package mx.edu.itsm.msc.agenteNotificador;
 
-import gnu.prolog.term.AtomTerm;
-import gnu.prolog.vm.Environment;
-import gnu.prolog.vm.Interpreter;
-import gnu.prolog.vm.PrologException;
 import jade.core.behaviours.Behaviour;
 
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import com.ugos.jiprolog.engine.JIPEngine;
+import com.ugos.jiprolog.engine.JIPQuery;
+import com.ugos.jiprolog.engine.JIPSyntaxErrorException;
+import com.ugos.jiprolog.engine.JIPTerm;
+import com.ugos.jiprolog.engine.JIPTermParser;
 
 /**
  *
@@ -31,79 +29,13 @@ public class Notificador extends Behaviour {
     private ObjectOutputStream write;
     
     public void inid(){
-        Environment ev = new Environment();
-        ev.ensureLoaded(AtomTerm.get(""));
-        Interpreter it = ev.createInterpreter();
-        
-        ev.runInitialization(it);
-        
-        try {
-			it.runOnce(null);
-		} catch (PrologException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-        //Query.hasSolution("use_module(library(jpl))"); // only because we call e.g. jpl_pl_syntax/1 below
-	
-        String t1 = "consult('reglas.pl')";
-        //System.out.println(t1 + " " + (Query.hasSolution(t1) ? "succeeded" : "failed"));
-                
         
     }
 
-    private void initSocket(){
-        try {
-            System.out.println("abriendo socket");
-            server=new ServerSocket(port);
-            Socket client=server.accept();
-            reader=new ObjectInputStream(client.getInputStream());
-            write=new ObjectOutputStream(client.getOutputStream());     
-            
-            System.out.println("socket abierto");
-        } catch (IOException ex) {
-            Logger.getLogger(Notificador.class.getName()).log(Level.SEVERE, null, ex);
-            
-        }
-    }
-    public Notificador() {
-        inid();
-        initSocket();
-    }
+    
     
     @Override
     public void action() {
-        //try {
-//            Object entrada;
-//            entrada=reader.readObject();
-//            System.out.println(entrada.toString());
-//            
-//            String t1 = "consult('reglas.pl')";
-//            //System.out.println(t1 + " " + (Query.hasSolution(t1) ? "succeeded" : "failed"));
-//            
-//            String resultado="";
-//            String t4 = "transaccion(X,"+entrada.toString()+")";
-		//System.out.println("first solution of " + t4 + ": X = " + Query.oneSolution(t4).get("Y"));
-//		Map<String, Term>[] ss4 = Query.allSolutions(t4);
-//		for (int i = 0; i < ss4.length; i++) {
-//			resultado+=(ss4[i].get("X"))+",";
-//			System.out.println("X = " + ss4[i]);
-//		}
-//        
-//            write.writeObject(resultado);
-//        
-//        } catch (IOException ex) {
-//            Logger.getLogger(Comportamiento.class.getName()).log(Level.SEVERE, null, ex);
-//            initSocket();
-//        } catch (PrologException pex) {
-//            try {
-//                write.writeObject("");
-//            } catch (IOException ex) {
-//                Logger.getLogger(Notificador.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(Notificador.class.getName()).log(Level.SEVERE, null, ex);
-//        } 
     }
 
     @Override
@@ -111,4 +43,39 @@ public class Notificador extends Behaviour {
         return false;
     }
     
+    public static void main(String[] args) {
+    	// New instance of Prolog engine
+    	JIPEngine jip = new JIPEngine();
+
+    	JIPTerm query = null;
+
+    	try
+    	{            
+    	    jip.compileFile("./reglas.pl");
+    	    jip.loadFile("reglas.jip");
+    		// parse query
+    	    JIPTermParser parser = jip.getTermParser();
+    	    query = parser.parseTerm("transacion(X,150014)");
+    	}
+    	catch(JIPSyntaxErrorException ex)
+    	{
+    	    // there is a syntax error in the query
+    	    ex.printStackTrace();
+    	    System.exit(0);
+    	}
+
+    	// open a synchronous query
+    	JIPQuery jipQuery = jip.openSynchronousQuery(query);
+    	JIPTerm solution;
+
+    	
+    	// Loop while there is another solution
+    	while (jipQuery.hasMoreChoicePoints())
+    	{
+    	    solution = jipQuery.nextSolution();
+    	    System.out.println(solution);
+    	}   
+         
+        
+	}
 }
